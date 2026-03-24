90defd8fc1ff20349c8bc6afc6a1e52ad672c8e2
Albu, Ovidiu-Teodor <ovidiu-teodor.albu@teliacompany.com>
2026-03-24 20:02:59 +0200

    css nou aplicat

--- src/app/library-component/library-component.ts
+++ src/app/library-component/library-component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-library-component',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './library-component.html',
   styleUrl: './library-component.css',
 })
