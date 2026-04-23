f98a544bf8022d248f85f9326bac77fe9c269ef2
Albu, Ovidiu-Teodor <ovidiu-teodor.albu@teliacompany.com>
2026-04-23 19:54:20 +0300

    send message nu merge

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,8 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter, withInMemoryScrolling } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -13,6 +13,8 @@ export const appConfig: ApplicationConfig = {
         anchorScrolling: 'enabled',
         scrollPositionRestoration: 'top',
       })
-    ), provideClientHydration(withEventReplay())
+    ),
+    provideClientHydration(withEventReplay()),
+    provideHttpClient(withFetch())
   ]
 };
