b4b38951e199e351c63d150d4688b5c046de6e02
Albu, Ovidiu-Teodor <ovidiu-teodor.albu@teliacompany.com>
2026-04-23 18:42:54 +0300

    prerandare pentru rutele diamnice

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,25 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'article/:id',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: async () => [
+      { id: '1' },
+      { id: '2' },
+      { id: '3' }
+    ]
+  },
+  {
+    path: 'course/:id',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: async () => [
+      { id: '1' },
+      { id: '2' },
+      { id: '3' },
+      { id: '4' }
+    ]
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
